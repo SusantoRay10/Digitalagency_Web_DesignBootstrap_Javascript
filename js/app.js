6d48bb0c1cfe2a0fe22b69483dc06a5e2f0fd7c9
SusantoRay10 <csesusanto@gmail.com>
2022-07-26 21:32:35 +0600

    First Commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(function(){
+
+    /*----------Navbar Sticky ---------- */
+    $(window).on('scroll',function(){
+        var scroll = $(window).scrollTop();
+        if(scroll >= 50){
+            $('.hamburger').addClass('whenscrolled');
+            $('nav').addClass('stickyadd');
+            $('nav').removeClass('stick');
+
+        }
+        else{
+            $('.hamburger').removeClass('whenscrolled');
+            $('nav').removeClass('stickyadd');
+            $('nav').addClass('stick');
+        }
+    })
+
+    /*------------Smooth Scroll Effect-----------*/
+    $('.navbar-nav a[href^="#"]').on('click',function(event){
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop:$(this.hash).offset().top
+
+        },1000);
+    });
+
+
+
+  
+
+    /**--------scroll animation effect */
+    AOS.init();
+});
